aa9e3aafb1f05eed379901498994d2a865ef6e10
FabianEscobar <fabiannorbertoescobar@gmail.com>
2017-09-18 17:44:53 -0300

    Initial commit

---
+++ src/mochila/Main.java
@@ -0,0 +1,5 @@
+package mochila;
+
+public class Main {
+
+}
---
+++ src/mochila/Mochila.java
@@ -0,0 +1,17 @@
+package mochila;
+
+import java.io.File;
+
+public class Mochila extends EjercicioOIA {
+
+	public Mochila(File entrada, File salida) {
+		super(entrada, salida);
+
+	}
+
+	@Override
+	public void resolver() {
+		
+	}
+
+}
